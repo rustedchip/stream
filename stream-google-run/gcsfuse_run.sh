5ac0150a76d4b2c45e79171cb00e74b573cf45fd
rustedchip <rustedchip@gmail.com>
2023-03-30 20:43:39 -0700

    gcsfuse

--- stream-google-run/gcsfuse_run.sh
+++ stream-google-run/gcsfuse_run.sh
@@ -2,12 +2,22 @@
 set -eo pipefail
 mkdir -p $MNT_DIR
 
+echo "im here carnal"
+ls -la /var/www/
+ls -la $MNT_DIR
+
 echo "mounting-gcs-fuse"
 gcsfuse -o allow-other --uid 33 --gid 33 --debug_gcs --debug_fuse  $BUCKET $MNT_DIR 
 echo "mounting-has-been-completed"
+echo "im here carnal"
+ls -la /var/www/
+ls -la $MNT_DIR
 
 chmod 777 -R $MNT_DIR
 chown -R www-data:www-data $MNT_DIR
+echo "im here carnal"
+ls -la /var/www/
+ls -la $MNT_DIR
 
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
