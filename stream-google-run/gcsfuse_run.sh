9a360b1a9c3bc7b4c00c9850b830c80c79733935
rustedchip <rustedchip@gmail.com>
2023-03-30 19:09:31 -0700

    gcsfuse

--- stream-google-run/gcsfuse_run.sh
+++ stream-google-run/gcsfuse_run.sh
@@ -8,6 +8,9 @@ echo "Mounting GCS Fuse."
 gcsfuse --debug_gcs --debug_fuse $BUCKET $MNT_DIR
 echo "Mounting completed."
 
+chmod 777 -R $MNT_DIR
+chown -R www-data:www-data $MNT_DIR
+
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
         set -- apache2-foreground "$@"
