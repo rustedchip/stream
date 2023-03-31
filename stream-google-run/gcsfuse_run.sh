0a6f3ec90f158388a8d3e3c89ba32009722e2201
rustedchip <rustedchip@gmail.com>
2023-03-30 21:54:42 -0700

    gcsfuse

--- stream-google-run/gcsfuse_run.sh
+++ stream-google-run/gcsfuse_run.sh
@@ -2,7 +2,7 @@
 set -eo pipefail
 mkdir -p $MNT_DIR
 chown -R www-data:www-data $MNT_DIR
-chmod chmod 777 -R $MNT_DIR
+chmod 777 -R $MNT_DIR
 
 echo "mounting-gcs-fuse"
 gcsfuse --dir-mode 777 --file-mode 777 --debug_gcs --debug_fuse  $BUCKET $MNT_DIR 
