7b60cb624dddfc8bcfb881d87adada37458bf856
rustedchip <rustedchip@gmail.com>
2023-06-11 02:09:53 -0700

    search-engine :: full-text-search

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -38,18 +38,7 @@ class AppController extends Controller
     }
     public function stream_search_results($query)
     {
-        /* $search = str_replace("+", " ",  $query);
-        $stream = Post::where('content', 'LIKE', '%' . $search . '%')->orderBy('created_at', 'desc')->paginate(25);
-        if ($stream->isEmpty()) {
-            Session::flash('message', 'Search has no Results.');
-        }
-        return view('home', compact('stream', 'search'));
-
-        */
-
-
         $search = str_replace("+", " ",  $query);
-
         $terms = explode("+", $query);
         $posts = array();
 
